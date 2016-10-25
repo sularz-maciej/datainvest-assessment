2ade82ec4ee5e3b58752e2037c7fab6db27599dd
Maciej Sularz <sularz.maciej@gmail.com>
2016-10-25 18:36:56 +0200

    Finished initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function( grunt ) {
+
+	// Load grunt tasks
+	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		sass: {
+			dist: {
+				files: {
+					'src/css/main.css' : 'src/sass/main.scss'
+				}
+			}
+		},
+
+		watch: {
+			options: {
+			      livereload: true
+		    },
+			css: {
+				files: 'src/**/*.scss',
+				tasks: ['sass']
+			},
+			html: {
+				files: 'src/**/index.html'
+			}
+		}
+	});
+
+	grunt.registerTask( 'default', ['watch'] );
+}
